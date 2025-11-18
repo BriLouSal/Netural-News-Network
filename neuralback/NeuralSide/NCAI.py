4a36c8fbd92126a8bd3bf9016c7af1403bbf8ec8
Brian Louis <brianlouis.salinas116@gmail.com>
2025-11-17 20:50:18 -0700

    Added

--- neuralback/NeuralSide/NCAI.py
+++ neuralback/NeuralSide/NCAI.py
@@ -2,6 +2,7 @@
 #  Main AI Core
 from anthropic import AnthropicVertex
 from anthropic import Anthropic
+from dotenv import load_dotenv
 
 
 
@@ -11,3 +12,34 @@ from anthropic import Anthropic
 
 import json
 from json import JSONDecodeError
+
+# Json Parse
+
+load_dotenv()
+
+
+
+
+
+
+def parser():
+    pass
+
+def dataset():
+    pass
+
+
+
+def left_wing_bias():
+    negative_data = client.messages.create(
+    model=MODEL_AI,
+    max_tokens=int(MAX_TOKEN_FOR_NEWS_SENTIMENT),
+    messages =[
+        {"role": "user", 
+            "content": f"Generate a news for a stock  that has positivety rating of and create this news that would have catalyst such as that the sentiment rating is 0.0-0.50. Create hundreds of output"}
+    ],
+)
+    return negative_data.content[0].text.strip()
+
+def right_wing_bias():
+    pass
---
+++ neuralback/NeuralSide/bias_indicator.py
@@ -0,0 +1,11 @@
+from NCAI import left_wing_bias, right_wing_bias
+
+
+
+
+
+
+
+
+def training_bias_model():
+    pass
